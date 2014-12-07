16bcfb8dd9c336b8613c2b9bcfef540f2ac4aad5
Robin <imer@imer.cc>
2014-12-07 02:00:06 +0100

    Added Text class

---
+++ src/Text.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   Text.cpp
+ * Author: iMer
+ * 
+ * Created on 7. Dezember 2014, 00:48
+ */
+
+#include "Text.hpp"
+namespace engine{
+	Text::Text(Scene* scene): Node(scene) {
+	}
+
+	Text::~Text() {
+	}
+	
+	void Text::OnDraw(sf::RenderTarget& target, sf::RenderStates states, float delta){
+		target.draw(m_text, states);
+	}
+	bool Text::initialize(Json::Value& root){
+		if (!Node::initialize(root)){
+			return false;
+		}
+		auto text = root["text"];
+		if (text.isObject()){
+			m_font.loadFromFile(text.get("font", "").asString());
+			m_text.setFont(m_font);
+			m_text.setCharacterSize(text.get('size', 30).asInt());
+			auto color = text["color"];
+			if (color.isArray()){
+				m_text.setColor(sf::Color(color.get(0u, 0).asInt(), color.get(1u, 0).asInt(), color.get(2u, 0).asInt(), color.get(3u, 255).asInt()));
+			}
+			m_text.setString(text.get("text", "").asString());
+		}
+		return true;
+	}
+}
+
---
+++ src/Text.hpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   Text.hpp
+ * Author: iMer
+ *
+ * Created on 7. Dezember 2014, 00:48
+ */
+
+#ifndef ENGINE_TEXT_HPP
+#define	ENGINE_TEXT_HPP
+#include "Node.hpp"
+#include <SFML/Graphics.hpp>
+namespace engine{
+class Text: public Node {
+protected:
+    sf::Font m_font;
+    sf::Text m_text;
+public:
+    Text(Scene* scene);
+    virtual ~Text();
+    void SetText(std::string text){
+        m_text.setString(text);
+    }
+    std::string GetText() const{
+        m_text.getString();
+    }
+    sf::Text& GetSFText() {
+        return m_text;
+    }
+
+    void SetFont(sf::Font font) {
+        m_font = font;
+    }
+
+    sf::Font GetFont() const {
+        return m_font;
+    }
+    virtual bool initialize(Json::Value& root);
+
+protected:
+    virtual void OnDraw(sf::RenderTarget& target, sf::RenderStates states, float delta);
+
+};
+}
+
+#endif	/* TEXT_HPP */
+
