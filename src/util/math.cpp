07726f3677265d398da7fcca95b4fd9feca6b8b0
Robin Rolf <imer@imer.cc>
2015-08-22 17:41:28 +0200

    Angle helper function
    Cause i always have to look up how to do this shit

---
+++ src/util/math.cpp
@@ -0,0 +1,15 @@
+//
+// Created by iMer on 22.08.2015.
+//
+
+#include <Engine/util/math.hpp>
+namespace engine {
+	namespace util {
+		float b2AngleDeg(b2Vec2 from, b2Vec2 to) {
+			to-=from;
+			float a =  atan2(to.y, to.x) * 180 / fPI;
+			if (a < 0) a += 360;
+			return a;
+		}
+	}
+}
