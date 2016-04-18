1a63bb67d19be8dc5c579a0246a679a7e563e918
Robin Rolf <imer@imer.cc>
2016-04-18 13:46:16 +0200

    Fix GCC syntax error
    VC is more forgiving apparently

--- src/util/json.cpp
+++ src/util/json.cpp
@@ -2,63 +2,63 @@
 
 namespace engine {
 
-float jsonNodeAs<float>(const Json::Value& node) {
+template <> float jsonNodeAs<float>(const Json::Value& node) {
     if (node.isNull()) {
         return 0;
     }
     return node.asFloat();
 }
 
-double jsonNodeAs<double>(const Json::Value& node) {
+template <> double jsonNodeAs<double>(const Json::Value& node) {
     if (node.isNull()) {
         return 0;
     }
     return node.asDouble();
 }
 
-int jsonNodeAs<int>(const Json::Value& node) {
+template <> int jsonNodeAs<int>(const Json::Value& node) {
     if (node.isNull()) {
         return 0;
     }
     return node.asInt();
 }
 
-unsigned int jsonNodeAs<unsigned int>(const Json::Value& node) {
+template <> unsigned int jsonNodeAs<unsigned int>(const Json::Value& node) {
     if (node.isNull()) {
         return 0;
     }
     return node.asUInt();
 }
 
-int64_t jsonNodeAs<int64_t>(const Json::Value& node) {
+template <> int64_t jsonNodeAs<int64_t>(const Json::Value& node) {
     if (node.isNull()) {
         return 0;
     }
     return node.asInt64();
 }
 
-uint64_t jsonNodeAs<uint64_t>(const Json::Value& node) {
+template <> uint64_t jsonNodeAs<uint64_t>(const Json::Value& node) {
     if (node.isNull()) {
         return 0;
     }
     return node.asUInt64();
 }
 
-bool jsonNodeAs<bool>(const Json::Value& node) {
+template <> bool jsonNodeAs<bool>(const Json::Value& node) {
     if (node.isNull()) {
         return false;
     }
     return node.asBool();
 }
 
-const char* jsonNodeAs<const char*>(const Json::Value& node) {
+template <> const char* jsonNodeAs<const char*>(const Json::Value& node) {
     if (node.isNull()) {
         return nullptr;
     }
     return node.asCString();
 }
 
-std::string jsonNodeAs<std::string>(const Json::Value& node) {
+template <> std::string jsonNodeAs<std::string>(const Json::Value& node) {
     if (node.isNull()) {
         return "";
     }
