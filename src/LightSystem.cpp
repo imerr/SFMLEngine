fe183526a171ed39d9c693359ab2b36ae9121859
Robin Rolf <imer@imer.cc>
2016-09-21 05:18:11 +0200

    LightSystem: Just don't blur if shaders aren't available
    ..instead of disabling lighting

--- src/LightSystem.cpp
+++ src/LightSystem.cpp
@@ -27,11 +27,13 @@ namespace engine {
 
     LightSystem::LightSystem(Scene* scene) : m_scene(scene), m_ambientColor(sf::Color::White), m_needsUpdate(true), m_enabled(true) {
 		if (!sf::Shader::isAvailable()) {
+			std::cerr << "Shaders are not available" << std::endl;
 			m_available = false;
-			return;
+		} else {
+			m_available = true;
+        	m_blurShader.loadFromFile("assets/shader/fullpass.vert", "assets/shader/blur.frag");
+
 		}
-		m_available = true;
-        m_blurShader.loadFromFile("assets/shader/fullpass.vert", "assets/shader/blur.frag");
         auto size = m_scene->GetGame()->GetWindow()->getView().getSize();
         m_buffer.create(size.x, size.y);
         m_pingPongBuffer.create(size.x, size.y);
@@ -47,7 +49,7 @@ namespace engine {
     }
 
     void LightSystem::draw(sf::RenderTarget& target, sf::RenderStates states) {
-        if (!m_enabled || !m_available) {
+        if (!m_enabled) {
             return;
         }
 		std::lock_guard<std::recursive_mutex> lg(m_drawLock);
@@ -60,16 +62,18 @@ namespace engine {
                 light->DrawLight(m_buffer, states);
             });
             m_buffer.display();
-            auto textureSize = m_buffer.getSize();
-            for (size_t i = 0; i < blurPasses; i++) {
-                m_blurShader.setParameter("offsetFactor", sf::Vector2f(0.0f, 2.f / textureSize.y));
-                m_blurShader.setParameter("source", m_buffer.getTexture());
-                applyShader(m_blurShader, m_pingPongBuffer);
-                m_blurShader.setParameter("offsetFactor", sf::Vector2f(2.0f / textureSize.x, 0.0f));
-                m_blurShader.setParameter("source", m_pingPongBuffer.getTexture());
-                applyShader(m_blurShader, m_buffer);
-                m_buffer.display();
-            }
+			if (m_available) {
+				auto textureSize = m_buffer.getSize();
+				for (size_t i = 0; i < blurPasses; i++) {
+					m_blurShader.setParameter("offsetFactor", sf::Vector2f(0.0f, 2.f / textureSize.y));
+					m_blurShader.setParameter("source", m_buffer.getTexture());
+					applyShader(m_blurShader, m_pingPongBuffer);
+					m_blurShader.setParameter("offsetFactor", sf::Vector2f(2.0f / textureSize.x, 0.0f));
+					m_blurShader.setParameter("source", m_pingPongBuffer.getTexture());
+					applyShader(m_blurShader, m_buffer);
+					m_buffer.display();
+				}
+			}
         }
         sf::Transformable tr;
         auto window = m_scene->GetGame()->GetWindow();
